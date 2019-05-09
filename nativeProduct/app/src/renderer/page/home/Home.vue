d06e16fd4f1d9abd1feb88b314f39d5e86fcb434
TongGuan Lee <284377458@qq.com>
2019-05-09 19:54:30 +0800

    将page文件夹移除  内部文件夹移出

---
+++ nativeProduct/app/src/renderer/page/home/Home.vue
@@ -0,0 +1,26 @@
+<style lang="less"> 
+@home : ~"app-home";
+.@{home} {
+    height: 100%;
+    display: flex; 
+    background-color: rgba(0, 0, 0, 0.067) 
+    // flex-flow: <flex-direction> || <flex-wrap>;
+}
+
+</style>
+<template>
+  <div :class="prefix">
+
+  </div>
+</template>
+<script> 
+
+const prefix = "app-home"; 
+export default { 
+  data() {
+    return {
+      prefix: prefix, 
+    };
+  }
+};
+</script>
---
+++ nativeProduct/app/src/renderer/page/home/index.js
@@ -0,0 +1,5 @@
+import Vue from "vue" 
+import Home from "./Home.vue";
+new Vue({
+    render: h => h(Home)
+}).$mount("#root"); 

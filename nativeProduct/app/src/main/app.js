ba58dbc7040c0e3c0adb2a111da49c36799b5b20
TongGuan Lee <284377458@qq.com>
2019-05-22 08:20:04 +0800

    删除main.js  添加app.js

---
+++ nativeProduct/app/src/main/app.js
@@ -0,0 +1,29 @@
+let { app, BrowserWindow } = require("electron") ; 
+app.commandLine.appendSwitch("--disable-background-timer-throttling");  
+
+let win; 
+const createWindow = ()=>{
+    win = new BrowserWindow({ width: 1000, height: 600, webPreferences: { devTools: true ,nodeIntegration : true, nodeIntegrationInWorker: true}, frame: false}); 
+    win.loadFile("./dist/renderer/home.html");  
+    win.once('closed', () => { 
+        win = null
+    });
+} 
+
+app.on('window-all-closed', () => { 
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+});
+  
+app.on('activate', () => { 
+    if (mainWindow === null) {
+        createWindow()
+    }
+}); 
+
+app.once("ready", createWindow);
+
+ 
+ 
+   
